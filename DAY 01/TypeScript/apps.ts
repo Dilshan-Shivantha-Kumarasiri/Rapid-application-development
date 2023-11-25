55060cf49e3914e637f957cba1f917c1baf332cb
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-11-25 12:12:49 +0530

    primitive and function

--- DAY 01/TypeScript/apps.ts
+++ DAY 01/TypeScript/apps.ts
@@ -1,8 +1,2 @@
 // function
 
-function findSum(num1:number,num2:number):number {
-    return num1+num2; // need to return the number if not show some error in source code in type script
-}
-
-let result = findSum(10,20);
-console.log("function type return value = "+result);
