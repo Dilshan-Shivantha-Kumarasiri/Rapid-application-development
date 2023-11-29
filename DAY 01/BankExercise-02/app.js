64dad3fbc683eb04555ab0394bb02a644d38c5a5
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-11-29 14:40:04 +0530

    bank exercise 02

--- DAY 01/BankExercise-02/app.js
+++ DAY 01/BankExercise-02/app.js
@@ -1,18 +1,24 @@
 var name_input_element = document.getElementById('ownerName');
 var fixedAmountElement = document.getElementById('fixedAmount');
 var addFixedBtn = document.getElementById('addFixBtn');
+var tableContent = document.getElementById("fixedDepositTableBody");
+var interest_value = document.getElementById("newInterestRate");
 addFixedBtn.addEventListener('click', function () {
     var ownerName = name_input_element.value;
     var fixedAmount = fixedAmountElement.value;
     console.log("owner : ".concat(ownerName, " and fixed value is ").concat(fixedAmount));
-    var tableRow = "<tr><td>".concat(ownerName, "</td><td>").concat(fixedAmount, "</td><td><td></tr>");
-    var tableContent = document.getElementById("fixedDepositTableBody");
+    var tableRow = "<tr><td>".concat(ownerName, "</td><td>").concat(fixedAmount, "</td><td>0<td><td>0<td></tr>");
     var htmlTableRowElement = tableContent.appendChild(document.createElement("tr"));
     htmlTableRowElement.innerHTML = tableRow;
     // get row count in tbody
     var childElementCount = tableContent.childElementCount;
-    console.log(childElementCount);
+    // add table to interest rate
+    var childRow = tableContent.children.item(0).children.item(2).innerHTML;
+    console.log(childRow);
     // enter cells
     // let htmlTableCellElement = htmlTableRowElement.insertCell(3);
     // htmlTableCellElement.innerText = "5"
+});
+document.getElementById('updateInterestBtn').addEventListener("click", function () {
+    var childRow = tableContent.children.item(0).children.item(2).innerHTML = interest_value.value;
 });
--- DAY 01/BankExercise-02/app.ts
+++ DAY 01/BankExercise-02/app.ts
@@ -3,30 +3,35 @@ let fixedAmountElement = document.getElementById('fixedAmount') as HTMLInputElem
 
 let addFixedBtn = document.getElementById('addFixBtn');
 
-
-
+let tableContent = document.getElementById("fixedDepositTableBody");
+let interest_value = document.getElementById("newInterestRate") as HTMLInputElement;
 addFixedBtn.addEventListener('click',function () {
 
     let ownerName = name_input_element.value;
     let fixedAmount = fixedAmountElement.value;
     console.log(`owner : ${ownerName} and fixed value is ${fixedAmount}`);
 
-    let tableRow = `<tr><td>${ownerName}</td><td>${fixedAmount}</td><td><td></tr>`
+    let tableRow = `<tr><td>${ownerName}</td><td>${fixedAmount}</td><td>0<td><td>0<td></tr>`
 
-
-    let tableContent = document.getElementById("fixedDepositTableBody");
     let htmlTableRowElement = tableContent.appendChild(document.createElement("tr"));
     htmlTableRowElement.innerHTML = tableRow;
 
     // get row count in tbody
     let childElementCount = tableContent.childElementCount;
-    console.log(childElementCount);
+
+    // add table to interest rate
+    let childRow = tableContent.children.item(0).children.item(2).innerHTML;
+    console.log(childRow)
 
     // enter cells
     // let htmlTableCellElement = htmlTableRowElement.insertCell(3);
     // htmlTableCellElement.innerText = "5"
 
-
-
-
 });
+
+document.getElementById('updateInterestBtn').addEventListener("click",function () {
+
+
+    let childRow = tableContent.children.item(0).children.item(2).innerHTML = interest_value.value;
+
+})
