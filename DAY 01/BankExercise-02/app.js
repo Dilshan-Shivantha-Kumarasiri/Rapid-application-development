ba371ea5523fa7f096af078a0236c89eedbea0cf
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-11-29 13:27:09 +0530

    bank exercise 02

--- DAY 01/BankExercise-02/app.js
+++ DAY 01/BankExercise-02/app.js
@@ -5,8 +5,14 @@ addFixedBtn.addEventListener('click', function () {
     var ownerName = name_input_element.value;
     var fixedAmount = fixedAmountElement.value;
     console.log("owner : ".concat(ownerName, " and fixed value is ").concat(fixedAmount));
-    var tableRow = "<tr><td>".concat(ownerName, "</td><td>").concat(fixedAmount, "</td></tr>");
+    var tableRow = "<tr><td>".concat(ownerName, "</td><td>").concat(fixedAmount, "</td><td><td></tr>");
     var tableContent = document.getElementById("fixedDepositTableBody");
     var htmlTableRowElement = tableContent.appendChild(document.createElement("tr"));
     htmlTableRowElement.innerHTML = tableRow;
+    // get row count in tbody
+    var childElementCount = tableContent.childElementCount;
+    console.log(childElementCount);
+    // enter cells
+    // let htmlTableCellElement = htmlTableRowElement.insertCell(3);
+    // htmlTableCellElement.innerText = "5"
 });
--- DAY 01/BankExercise-02/app.ts
+++ DAY 01/BankExercise-02/app.ts
@@ -11,12 +11,20 @@ addFixedBtn.addEventListener('click',function () {
     let fixedAmount = fixedAmountElement.value;
     console.log(`owner : ${ownerName} and fixed value is ${fixedAmount}`);
 
-    let tableRow = `<tr><td>${ownerName}</td><td>${fixedAmount}</td></tr>`
+    let tableRow = `<tr><td>${ownerName}</td><td>${fixedAmount}</td><td><td></tr>`
 
 
     let tableContent = document.getElementById("fixedDepositTableBody");
     let htmlTableRowElement = tableContent.appendChild(document.createElement("tr"));
     htmlTableRowElement.innerHTML = tableRow;
+
+    // get row count in tbody
+    let childElementCount = tableContent.childElementCount;
+    console.log(childElementCount);
+
+    // enter cells
+    // let htmlTableCellElement = htmlTableRowElement.insertCell(3);
+    // htmlTableCellElement.innerText = "5"
 
 
 
