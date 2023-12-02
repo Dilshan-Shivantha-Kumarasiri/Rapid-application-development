8bc91fd745bf16a3a9f12692fcebd9a059a00bc9
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-02 10:06:32 +0530

    bank exercise

--- DAY 01/BankExercise/app.js
+++ DAY 01/BankExercise/app.js
@@ -8,13 +8,40 @@ var Account = /** @class */ (function () {
     return Account;
 }());
 var rate = 12.5;
+var accounts = [];
 var name_input_element = document.getElementById('ownerName');
 var fixedAmountElement = document.getElementById('fixedAmount');
+var newInterestRate = document.getElementById('newInterestRate');
 var addFixedBtn = document.getElementById('addFixBtn');
+var table_body = document.getElementById("fixedDepositTableBody");
 addFixedBtn.addEventListener('click', function () {
     var ownerName = name_input_element.value;
     var fixedAmount = fixedAmountElement.value;
     var annualInterestRate = (rate * +fixedAmount) / 100;
     var account = new Account(ownerName, fixedAmount, rate, annualInterestRate);
-    console.log("owner : ".concat(ownerName, " and fixed value is ").concat(fixedAmount));
+    accounts.push(account);
+    console.log(accounts);
+    updateTable();
+});
+function updateTable() {
+    table_body.innerHTML = "";
+    accounts.map(function (r) {
+        var record_element = "<td>".concat(r.owner_name, "</td><td>").concat(r.fixed_amount, "</td> <td>").concat(r.interest_rate, "</td><td>").concat(r.annual_interest, "</td>");
+        var table_row = document.createElement("tr");
+        table_row.innerHTML = record_element;
+        table_body.appendChild(table_row);
+    });
+}
+//update btn
+document.getElementById("updateInterestBtn").addEventListener("click", function () {
+    var number = 0;
+    // accounts.map(r =>{
+    //     table_body.children.item(number).children.item(2).innerHTML = `<td>${r.interest_rate}</td>`
+    //     number+=number+1
+    // })
+    for (var i = 0; i < accounts.length; i++) {
+        // table_body.children.item(i).children.item(i).textContent;
+        var annualInterestRate = (+newInterestRate.value * accounts.at(i).fixed_amount) / 100;
+        var newAccounts = [];
+    }
 });
--- DAY 01/BankExercise/app.ts
+++ DAY 01/BankExercise/app.ts
@@ -15,11 +15,15 @@ class Account {
 }
 
 let rate = 12.5;
+let accounts: Account[] = []
 
 let name_input_element =document.getElementById('ownerName') as HTMLInputElement;
 let fixedAmountElement = document.getElementById('fixedAmount') as HTMLInputElement;
+let newInterestRate = document.getElementById('newInterestRate') as HTMLInputElement;
 
 let addFixedBtn = document.getElementById('addFixBtn');
+let table_body = document.getElementById("fixedDepositTableBody");
+
 
 
 
@@ -30,7 +34,45 @@ addFixedBtn.addEventListener('click',function () {
     let annualInterestRate = (rate * +fixedAmount)/100
 
     let account = new Account(ownerName,fixedAmount,rate,annualInterestRate);
+    accounts.push(account);
 
-    console.log(`owner : ${ownerName} and fixed value is ${fixedAmount}`)
+    console.log(accounts)
+    updateTable();
+});
 
+function updateTable() {
+
+    table_body.innerHTML = ""
+
+    accounts.map(r => { // iterating the accounts array
+        let record_element = `<td>${r.owner_name}</td><td>${r.fixed_amount}</td> <td>${r.interest_rate}</td><td>${r.annual_interest}</td>`
+        let table_row = document.createElement("tr");
+        table_row.innerHTML = record_element;
+        table_body.appendChild(table_row);
+
+    })
+}
+
+//update btn
+
+document.getElementById("updateInterestBtn").addEventListener("click",function () {
+
+    let number = 0;
+
+    // accounts.map(r =>{
+    //     table_body.children.item(number).children.item(2).innerHTML = `<td>${r.interest_rate}</td>`
+    //     number+=number+1
+    // })
+
+
+    for (let i = 0; i<accounts.length; i++){
+        // table_body.children.item(i).children.item(i).textContent;
+        let annualInterestRate = (+newInterestRate.value * accounts.at(i).fixed_amount)/100
+        let newAccounts: Account[] = []
+
+
+        
+
+
+    }
 });
