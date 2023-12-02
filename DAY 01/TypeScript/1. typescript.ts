d05e5accdc24feef0e9c2466b485393b7e9d765e
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-02 10:40:26 +0530

    bank exercise

--- DAY 01/TypeScript/1. typescript.ts
+++ DAY 01/TypeScript/1. typescript.ts
@@ -206,7 +206,30 @@ enum Roles {
     SUPER_ADMIN,
     ADMIN,
     MANAGER,
-    SELLER
+    SELLER,
+    BUYER
 }
 
+let myRole: Roles = Roles.ADMIN;
+
+function sendMessage(role: Roles) {
+    switch (role) {
+        case Roles.SUPER_ADMIN:
+            console.log("Hello, Super Admin");
+            break;
+        case Roles.ADMIN:
+            console.log("Hello, Admin");
+            break;
+        case Roles.MANAGER:
+            console.log("Hello, Manager");
+            break;
+        case Roles.SELLER:
+            console.log("Hello, Seller");
+            break;
+        default:
+            console.log("Hello, buyer");
+    }
+}
+
+sendMessage(Roles.SELLER);
 
