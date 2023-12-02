587d7be27a49884082792179405b326bfefd23da
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-02 09:12:07 +0530

    bank exercise

--- DAY 01/BankExercise/app.js
+++ DAY 01/BankExercise/app.js
@@ -1,10 +1,20 @@
+var Account = /** @class */ (function () {
+    function Account(owner_name, fixed_amount, interest_rate, annual_interest) {
+        this.owner_name = owner_name;
+        this.fixed_amount = fixed_amount;
+        this.interest_rate = interest_rate;
+        this.annual_interest = annual_interest;
+    }
+    return Account;
+}());
+var rate = 12.5;
 var name_input_element = document.getElementById('ownerName');
 var fixedAmountElement = document.getElementById('fixedAmount');
 var addFixedBtn = document.getElementById('addFixBtn');
 addFixedBtn.addEventListener('click', function () {
     var ownerName = name_input_element.value;
     var fixedAmount = fixedAmountElement.value;
-    // console.log(ownerName);
-    // console.log(fixedAmount);
+    var annualInterestRate = (rate * +fixedAmount) / 100;
+    var account = new Account(ownerName, fixedAmount, rate, annualInterestRate);
     console.log("owner : ".concat(ownerName, " and fixed value is ").concat(fixedAmount));
 });
--- DAY 01/BankExercise/app.ts
+++ DAY 01/BankExercise/app.ts
@@ -1,3 +1,21 @@
+class Account {
+
+    owner_name;
+    fixed_amount;
+    interest_rate;
+    annual_interest;
+
+    constructor(owner_name,fixed_amount,interest_rate,annual_interest) {
+        this.owner_name = owner_name;
+        this.fixed_amount = fixed_amount;
+        this.interest_rate = interest_rate;
+        this.annual_interest = annual_interest;
+
+    }
+}
+
+let rate = 12.5;
+
 let name_input_element =document.getElementById('ownerName') as HTMLInputElement;
 let fixedAmountElement = document.getElementById('fixedAmount') as HTMLInputElement;
 
@@ -9,8 +27,10 @@ addFixedBtn.addEventListener('click',function () {
 
     let ownerName = name_input_element.value;
     let fixedAmount = fixedAmountElement.value;
-    // console.log(ownerName);
-    // console.log(fixedAmount);
+    let annualInterestRate = (rate * +fixedAmount)/100
+
+    let account = new Account(ownerName,fixedAmount,rate,annualInterestRate);
+
     console.log(`owner : ${ownerName} and fixed value is ${fixedAmount}`)
 
 });
