32088770161c9b6dad8bd7fa422372d48184951b
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-03 13:43:04 +0530

    introduction how typescript work and some examples

--- Type script youtube/Hitesh choudharya/01 intro/variableMe.js
+++ Type script youtube/Hitesh choudharya/01 intro/variableMe.js
@@ -3,3 +3,19 @@ var greeting = "hello dilshan";
 //greeting.concat() // only suggest string methods not methods related to any other type
 greeting.toLowerCase();
 console.log(greeting);
+// best practices using the type script
+var useAge = 55; // Example 05
+useAge.toFixed();
+// no need to declare annotate number if assign value in the single line
+// Type is automatically type inferred to the number in "Example 05" no need to use ":number"
+var age; // Example 06
+age = 5;
+age = "5";
+// In the example 06 you can the variable value is not directly assign to the variable in single line
+// if we want to add behaviour that variable must need to assign number values we must need to use ":number" annotation
+// "age:number = 5 " this is not work. we need to assign in the single variable like "Example 07"
+var number;
+number = 5;
+// number = "7"; // can not assign string value
+var boolean = true;
+boolean.valueOf();
--- Type script youtube/Hitesh choudharya/01 intro/variableMe.ts
+++ Type script youtube/Hitesh choudharya/01 intro/variableMe.ts
@@ -4,3 +4,28 @@ let greeting :string  = "hello dilshan";
 greeting.toLowerCase();
 
 console.log(greeting);
+
+
+
+// best practices using the type script
+let useAge :number = 55; // Example 05
+useAge.toFixed();
+
+// no need to declare annotate number if assign value in the single line
+// Type is automatically type inferred to the number in "Example 05" no need to use ":number"
+
+let age; // Example 06
+age = 5;
+age = "5";
+
+// In the example 06 you can the variable value is not directly assign to the variable in single line
+// if we want to add behaviour that variable must need to assign number values we must need to use ":number" annotation
+// "age:number = 5 " this is not work. we need to assign in the single variable like "Example 07"
+
+let number : number;
+number = 5;
+// number = "7"; // can not assign string value
+
+
+let boolean :boolean = true;
+boolean.valueOf();
